f2bed691ad7390a6ba4e2417f2a1903922d3eb7e
lunactic <lunactic@gmail.com>
2015-10-08 10:54:55 +0200

    removed all statics, made a class
    Reasoning is that it can be used easier this way with different base URLs

--- src/main/java/ch/unifr/diva/DivaServicesCommunicator.java
+++ src/main/java/ch/unifr/diva/DivaServicesCommunicator.java
@@ -1,27 +1,28 @@
 package ch.unifr.diva;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.*;
 import java.util.List;
 
 public class DivaServicesCommunicator {
+    private String serverUrl;
+    private HttpRequest httpRequest;
+
+    /**
+     * Initialize a new DivaServicesCommunicator class
+     *
+     * @param serverUrl base url to use (e.g. http://divaservices.unifr.ch)
+     */
+    public DivaServicesCommunicator(String serverUrl) {
+        this.serverUrl = serverUrl;
+        this.httpRequest = new HttpRequest();
+    }
 
     /**
      * uploads an image to the server
@@ -29,8 +30,8 @@ public class DivaServicesCommunicator {
      * @param image the image to upload
      * @return the md5Hash to use in future requests
      */
-    public static String uploadImage(BufferedImage image){
-        if(!checkImageOnServer(image)) {
+    public String uploadImage(BufferedImage image) {
+        if (!checkImageOnServer(image)) {
             String base64Image = ImageEncoding.encodeToBase64(image);
             Map<String, Object> highlighter = new HashMap();
 
@@ -40,75 +41,73 @@ public class DivaServicesCommunicator {
             request.put("highlighter", high);
             request.put("inputs", inputs);
             request.put("image", base64Image);
-            JSONObject result = executePost("http://127.0.0.1:8080/upload", request);
+            JSONObject result = HttpRequest.executePost(serverUrl + "/upload", request);
             return result.getString("md5");
-        }else{
+        } else {
             return ImageEncoding.encodeToMd5(image);
         }
 
     }
 
     /**
-     *
      * @param image
      * @return
      */
-    public static BufferedImage runOtsuBinarization(BufferedImage image){
+    public BufferedImage runOtsuBinarization(BufferedImage image) {
         Map<String, Object> highlighter = new HashMap();
         JSONObject request = new JSONObject();
         JSONObject high = new JSONObject(highlighter);
         JSONObject inputs = new JSONObject();
         request.put("highlighter", high);
         request.put("inputs", inputs);
-        addImageToRequest(image,request);
-        JSONObject result = executePost("http://divaservices.unifr.ch/imageanalysis/binarization/otsu", request);
-        String resImage = (String)result.get("image");
+        addImageToRequest(image, request);
+        JSONObject result = HttpRequest.executePost(serverUrl + "/imageanalysis/binarization/otsu", request);
+        String resImage = (String) result.get("image");
         return ImageEncoding.decodeBas64(resImage);
-
     }
 
     /**
      * Extracts interest points from an image
-     * @param image the image to extract points from
-     * @param detector the detector to use
-     * @param blurSigma the amount of blur applied
-     * @param numScales the number of scales
-     * @param numOctaves the number of octaves
-     * @param threshold the threshold
+     *
+     * @param image               the image to extract points from
+     * @param detector            the detector to use
+     * @param blurSigma           the amount of blur applied
+     * @param numScales           the number of scales
+     * @param numOctaves          the number of octaves
+     * @param threshold           the threshold
      * @param maxFeaturesPerScale the maximal number of features to use
      * @return A list of points, representing the interest points
      */
-    public static List<Point> runMultiScaleInterestPointDetection(BufferedImage image, String detector, float blurSigma, int numScales, int numOctaves, float threshold, int maxFeaturesPerScale){
+    public List<Point> runMultiScaleInterestPointDetection(BufferedImage image, String detector, float blurSigma, int numScales, int numOctaves, float threshold, int maxFeaturesPerScale) {
         DecimalFormat df = new DecimalFormat("0", DecimalFormatSymbols.getInstance(Locale.ENGLISH));
-        df.setMaximumFractionDigits(10); //340 = DecimalFormat.DOUBLE_FRACTION_DIGITS
+        df.setMaximumFractionDigits(10);
 
         JSONObject request = new JSONObject();
         JSONObject high = new JSONObject();
         JSONObject inputs = new JSONObject();
-        inputs.put("detector",detector);
-        inputs.put("blurSigma",blurSigma);
-        inputs.put("numScales",numScales);
-        inputs.put("numOctaves",numOctaves);
-        inputs.put("threshold",df.format(threshold));
-        inputs.put("maxFeaturesPerScale",maxFeaturesPerScale);
+        inputs.put("detector", detector);
+        inputs.put("blurSigma", blurSigma);
+        inputs.put("numScales", numScales);
+        inputs.put("numOctaves", numOctaves);
+        inputs.put("threshold", df.format(threshold));
+        inputs.put("maxFeaturesPerScale", maxFeaturesPerScale);
 
         request.put("highlighter", high);
         request.put("inputs", inputs);
         addImageToRequest(image, request);
         //System.out.println(request.toString());
 
-        JSONObject result = executePost("http://divaservices.unifr.ch/ipd/multiscale",request);
+        JSONObject result = HttpRequest.executePost(serverUrl + "/ipd/multiscale", request);
 
         return extractPoints(result);
     }
 
     /**
-     *
      * @param image
      * @param rectangle
      * @return A list of polygons, each representing a text line
      */
-    public static List<Rectangle> runHistogramTextLineExtraction(BufferedImage image, Rectangle rectangle){
+    public List<Rectangle> runHistogramTextLineExtraction(BufferedImage image, Rectangle rectangle) {
         Map<String, Object> highlighter = new HashMap();
         highlighter.put("segments", prepareRectangle(rectangle));
         highlighter.put("closed", true);
@@ -121,18 +120,17 @@ public class DivaServicesCommunicator {
         request.put("inputs", inputs);
         addImageToRequest(image, request);
 
-        JSONObject result = executePost("http://divaservices.unifr.ch/segmentation/textline/hist", request);
+        JSONObject result = HttpRequest.executePost(serverUrl + "/segmentation/textline/hist", request);
         // now you have the string representation of the HTML request
 
         return extractRectangles(result);
     }
 
     /**
-     *
      * @param image
      * @return
      */
-    public static BufferedImage runSauvolaBinarization(BufferedImage image) {
+    public BufferedImage runSauvolaBinarization(BufferedImage image) {
         Map<String, Object> highlighter = new HashMap();
 
         JSONObject request = new JSONObject();
@@ -141,8 +139,8 @@ public class DivaServicesCommunicator {
         request.put("highlighter", high);
         request.put("inputs", inputs);
         addImageToRequest(image, request);
-        JSONObject result = executePost("http://divaservices.unifr.ch/imageanalysis/binarization/sauvola", request);
-        String resImage = (String)result.get("image");
+        JSONObject result = HttpRequest.executePost(serverUrl + "/imageanalysis/binarization/sauvola", request);
+        String resImage = (String) result.get("image");
         return ImageEncoding.decodeBas64(resImage);
     }
 
@@ -151,7 +149,7 @@ public class DivaServicesCommunicator {
      * @param rectangle
      * @return
      */
-    public static List<Polygon> runSeamCarvingTextlineExtraction(BufferedImage image, Rectangle rectangle) {
+    public List<Polygon> runSeamCarvingTextlineExtraction(BufferedImage image, Rectangle rectangle) {
         Map<String, Object> highlighter = new HashMap();
         highlighter.put("segments", prepareRectangle(rectangle));
         highlighter.put("closed", true);
@@ -164,88 +162,39 @@ public class DivaServicesCommunicator {
         request.put("inputs", inputs);
         addImageToRequest(image, request);
 
-        JSONObject result = executePost("http://divaservices.unifr.ch/segmentation/textline/seam", request);
-        // now you have the string representation of the HTML request
+        JSONObject result = HttpRequest.executePost(serverUrl + "/segmentation/textline/seam", request);
 
         return extractPolygons(result);
     }
 
-    /**
-     * Executes a post request and returns the body json object
-     *
-     * @param url
-     * @param payload
-     * @return
-     */
-    private static JSONObject executePost(String url, JSONObject payload) {
-        HttpClient client = new DefaultHttpClient();
-        HttpPost post = new HttpPost(url);
-        try {
-            StringEntity se = new StringEntity(payload.toString());
-            se.setContentType("application/json");
-            post.setEntity(se);
-            HttpResponse response = client.execute(post);
-            HttpEntity entity = response.getEntity();
-            if (entity != null) {
-
-                // A Simple JSON Response Read
-                InputStream instream = entity.getContent();
-                JSONObject result = new JSONObject(convertStreamToString(instream));
-                instream.close();
-                return result;
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return null;
-    }
-
-    /**
-     * executes a GET request
-     * @param url
-     * @return
-     */
-    private static JSONObject executeGet(String url){
-        HttpClient client = new DefaultHttpClient();
-        HttpGet get = new HttpGet(url);
-        try {
-            HttpResponse response = client.execute(get);
-            HttpEntity entity = response.getEntity();
-            if (entity != null) {
-                // A Simple JSON Response Read
-                InputStream instream = entity.getContent();
-                JSONObject result = new JSONObject(convertStreamToString(instream));
-                instream.close();
-                return result;
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return null;
-
-    }
 
     /**
      * converts a Rectangle into a List<int[]> for sending it as JSON
+     *
      * @param rectangle the rectangle
      * @return a List<int[]>
      */
-    private static List<int[]> prepareRectangle(Rectangle rectangle){
+    private List<int[]> prepareRectangle(Rectangle rectangle) {
         List<int[]> points = new ArrayList<>();
+        //top left
         points.add(new int[]{rectangle.x, rectangle.y});
-        points.add(new int[]{rectangle.x + rectangle.width, rectangle.y});
-        points.add(new int[]{rectangle.x + rectangle.width, rectangle.y + rectangle.height});
+        //bottom left
         points.add(new int[]{rectangle.x, rectangle.y + rectangle.height});
+        //bottom right
+        points.add(new int[]{rectangle.x + rectangle.width, rectangle.y + rectangle.height});
+        //top right
+        points.add(new int[]{rectangle.x + rectangle.width, rectangle.y});
         return points;
 
     }
 
     /**
      * Creates polygons from polygons as "lines" in DivaServices
+     *
      * @param result the JSON result object from DIVAServices
      * @return a list of polygons
      */
-    private static List<Polygon> extractPolygons(JSONObject result){
+    private List<Polygon> extractPolygons(JSONObject result) {
         JSONArray highlighters = result.getJSONArray("highlighters");
         List<Polygon> polygons = new ArrayList<>();
         for (int i = 0; i < highlighters.length(); i++) {
@@ -263,10 +212,11 @@ public class DivaServicesCommunicator {
 
     /**
      * Creates a list of rectangles from "rectangles" returned from DIVAServices
+     *
      * @param result the JSONObject returned from DivaServices
      * @return A list of rectangles
      */
-    private static List<Rectangle> extractRectangles(JSONObject result){
+    private List<Rectangle> extractRectangles(JSONObject result) {
         JSONArray highlighters = result.getJSONArray("highlighters");
         List<Rectangle> rectangles = new ArrayList<>();
         for (int i = 0; i < highlighters.length(); i++) {
@@ -275,86 +225,60 @@ public class DivaServicesCommunicator {
             //get top left point
             JSONArray topLeft = segments.getJSONArray(0);
             JSONArray bottomRight = segments.getJSONArray(2);
-            Rectangle rectangle = new Rectangle(topLeft.getInt(0),topLeft.getInt(1),bottomRight.getInt(0)-topLeft.getInt(0),bottomRight.getInt(1) - topLeft.getInt(1));
+            Rectangle rectangle = new Rectangle(topLeft.getInt(0), topLeft.getInt(1), bottomRight.getInt(0) - topLeft.getInt(0), bottomRight.getInt(1) - topLeft.getInt(1));
             rectangles.add(rectangle);
         }
         return rectangles;
     }
 
 
-    private static List<Point> extractPoints(JSONObject result){
+    private List<Point> extractPoints(JSONObject result) {
         JSONArray highlighters = result.getJSONArray("highlighters");
         List<Point> points = new ArrayList<>();
         for (int i = 0; i < highlighters.length(); i++) {
             JSONObject line = highlighters.getJSONObject(i).getJSONObject("point");
             JSONArray position = line.getJSONArray("position");
             //get top left point
-            points.add(new Point(position.getInt(0),position.getInt(1)));
+            points.add(new Point(position.getInt(0), position.getInt(1)));
         }
         return points;
     }
 
     /**
-     * Converts an input stream to a json string
-     *
-     * @param is
-     * @return
-     */
-    private static String convertStreamToString(InputStream is) {
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb = new StringBuilder();
-
-        String line;
-        try {
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                is.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return sb.toString();
-    }
-
-    /**
      * TODO: Use this method in all image calls to reduce payload
-     *
+     * <p>
      * Checks if an image is available on the server
+     *
      * @param image The image
      * @return True if the image is already saved on the server, false otherwise
      */
-    private static boolean checkImageOnServer(BufferedImage image){
+    private boolean checkImageOnServer(BufferedImage image) {
         String md5 = ImageEncoding.encodeToMd5(image);
         String url = "http://divaservices.unifr.ch/image/" + md5;
-        JSONObject response = executeGet(url);
+        JSONObject response = HttpRequest.executeGet(url);
         return response.getBoolean("imageAvailable");
     }
 
     /**
      * checks if an image is available on the server and responds with the correct JSONObject for the request payload
+     *
      * @param image
      * @return
      */
-    private static JSONObject checkImage(BufferedImage image){
+    private JSONObject checkImage(BufferedImage image) {
         JSONObject result = new JSONObject();
-        if(!checkImageOnServer(image)){
+        if (!checkImageOnServer(image)) {
             result.put("image", ImageEncoding.encodeToBase64(image));
-        }else{
+        } else {
             result.put("md5Image", ImageEncoding.encodeToMd5(image));
         }
         return result;
     }
 
-    private static void addImageToRequest(BufferedImage image, JSONObject request){
+    private void addImageToRequest(BufferedImage image, JSONObject request) {
         JSONObject imageObj = checkImage(image);
-        for(String key : imageObj.keySet()){
-            request.put(key,imageObj.getString(key));
+        for (String key : imageObj.keySet()) {
+            request.put(key, imageObj.getString(key));
         }
     }
 }
--- src/test/java/ch/unifr/diva/DivaServicesCommunicatorTest.java
+++ src/test/java/ch/unifr/diva/DivaServicesCommunicatorTest.java
@@ -1,6 +1,7 @@
 package ch.unifr.diva;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import javax.imageio.ImageIO;
@@ -15,9 +16,15 @@ import static junit.framework.Assert.assertEquals;
  * Unit test for simple DivaServicesCommunicator.
  */
 public class DivaServicesCommunicatorTest{
+    private static DivaServicesCommunicator divaServicesCommunicator;
+
+    @BeforeClass
+    public static void beforeClass(){
+        divaServicesCommunicator = new DivaServicesCommunicator("http://divaservices.unifr.ch");
+    }
 
     @Before
-    public void beforTest(){
+    public void beforeTest(){
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
@@ -30,14 +37,14 @@ public class DivaServicesCommunicatorTest{
     public void testSeamCarving() throws IOException {
         BufferedImage image = ImageIO.read(new File("D:\\DEV\\UniFr\\DivaServicesCommunicator\\data\\d-008.jpg"));
         Rectangle rect = new Rectangle(141, 331, 1208, 404);
-        java.util.List<Polygon> response = DivaServicesCommunicator.runSeamCarvingTextlineExtraction(image, rect);
+        java.util.List<Polygon> response = divaServicesCommunicator.runSeamCarvingTextlineExtraction(image, rect);
         System.out.println("nr of polygons:" + response.size());
     }
 
     @Test
     public void testSauvolaBinarization() throws IOException {
         BufferedImage image = ImageIO.read(new File("D:\\DEV\\UniFr\\DivaServicesCommunicator\\data\\d-008.jpg"));
-        BufferedImage resImage = DivaServicesCommunicator.runSauvolaBinarization(image);
+        BufferedImage resImage = divaServicesCommunicator.runSauvolaBinarization(image);
         System.out.println("image size height: " + resImage.getHeight() + " - image size width: " + resImage.getWidth());
     }
 
@@ -45,21 +52,21 @@ public class DivaServicesCommunicatorTest{
     public void testHistogramTextLineExtraction() throws IOException {
         BufferedImage image = ImageIO.read(new File("D:\\DEV\\UniFr\\DivaServicesCommunicator\\data\\d-008.jpg"));
         Rectangle rect = new Rectangle(141, 331, 1208, 404);
-        java.util.List<Rectangle> response = DivaServicesCommunicator.runHistogramTextLineExtraction(image, rect);
+        java.util.List<Rectangle> response = divaServicesCommunicator.runHistogramTextLineExtraction(image, rect);
         System.out.println("nr of rectangles:" + response.size());
     }
 
     @Test
     public void testMultiScaleInterestPointDetection() throws IOException {
         BufferedImage image = ImageIO.read(new File("D:\\DEV\\UniFr\\DivaServicesCommunicator\\data\\d-008.jpg"));
-        java.util.List<Point> interestPoints = DivaServicesCommunicator.runMultiScaleInterestPointDetection(image, "Harris", 1.0f, 5, 3, 0.000001f, 2);
+        java.util.List<Point> interestPoints = divaServicesCommunicator.runMultiScaleInterestPointDetection(image, "Harris", 1.0f, 5, 3, 0.000001f, 2);
         System.out.println("nr of points: " + interestPoints.size());
     }
 
     @Test
     public void testOtsuBinarization() throws IOException {
         BufferedImage image = ImageIO.read(new File("D:\\DEV\\UniFr\\DivaServicesCommunicator\\data\\d-008.jpg"));
-        BufferedImage resImage = DivaServicesCommunicator.runOtsuBinarization(image);
+        BufferedImage resImage = divaServicesCommunicator.runOtsuBinarization(image);
         ImageIO.write(resImage, "png", new File("D:\\DEV\\UniFr\\DivaServicesCommunicator\\data\\d-008-out.png"));
         System.out.println("image size height: " + resImage.getHeight() + " - image size width: " + resImage.getWidth());
     }
@@ -68,14 +75,9 @@ public class DivaServicesCommunicatorTest{
     public void testUploadImage() throws IOException {
         BufferedImage image = ImageIO.read(new File("D:\\DEV\\UniFr\\DivaServicesCommunicator\\data\\d-008.jpg"));
         String sourceMd5 = ImageEncoding.encodeToMd5(image);
-        String targetMd5 = DivaServicesCommunicator.uploadImage(image);
+        String targetMd5 = divaServicesCommunicator.uploadImage(image);
         assertEquals(sourceMd5, targetMd5);
     }
 
-    @Test
-    public void testHistogramTextLineExtractionMdt() throws IOException {
-        BufferedImage image = ImageIO.read(new File("D:\\DEV\\UniFr\\DivaServicesCommunicator\\data\\d-008.jpg"));
-        String sourceMd5 = ImageEncoding.encodeToMd5(image);
-    }
 
 }
